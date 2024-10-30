cd3efeb85f4223e93e304389e9d6454bdba75c6e
nkansah-wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-10-29 23:58:31 -0500

    feat: Implement user endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CommentsModule } from './comments/comments.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    CommentsModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://wirekobrobbeyofficial:mrW1lar0IpJdclv3@cluster0.xrwoi.mongodb.net/threads?retryWrites=true&w=majority&appName=Cluster0',
+    ),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
